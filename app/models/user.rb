3225663c903bdfcc5546c7f723d547d9b2382faf
Anindita Palit <axp172130@users.noreply.github.com>
2020-03-05 10:27:21 -0600

    Log in commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  has_many :places
+  has_many :comments
+  has_many :photos
+        end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-config.action_mailer.default_url_options = {host: nomster-anindita-palit.herokuapp.com'}
+config.action_mailer.default_url_options = {host: 'anindita-palit-nomster.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
