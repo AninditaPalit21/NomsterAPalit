a36428a7420ae2f74f4917efe8dab61283fc023a
axp172130 <anindita.palit98@gmail.com>
2020-03-25 21:40:41 -0500

    Correction changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places do
-    resources :comments, :photos, only: :create
+  #resources :places do
+    #resources :comments, :photos, only: :create
   end
-  resources :users, only: :show
+  #resources :users, only: :show
 end
