d4e597041fe20771aa41da7f693d43d1786552c6
axp172130 <anindita.palit98@gmail.com>
2020-03-28 11:41:26 -0500

    Finished project  APalit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,11 +8,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   
   
-  if Rails.env.development?
-    storage :file
-  elsif Rails.end.production?
-    storage :aws
-  end
+  # if Rails.env.development?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
+
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIA5BRDRCEWDYZ3TT4C"],        # required
-    aws_secret_access_key: ENV["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"],
-            # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["nomsters3apalit"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
